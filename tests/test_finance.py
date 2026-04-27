88d5fd66dc103a909d611693bf96d3bf427bffaa
Syyal <laysbrasilino@gmail.com>
2026-04-27 15:38:28 -0300

    atualização teste

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -1,5 +1,10 @@
-from services.finance_service import load_data
+from services.finance_service import load_data, add_expense
 
 def test_load_data():
     data = load_data()
     assert isinstance(data, list)
+
+def test_add_expense():
+    add_expense(10, "teste")
+    data = load_data()
+    assert any(e["category"] == "teste" for e in data)
